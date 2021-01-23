5da70d125d07f765f7389f88435b8ffbf9255b5a
Anh Tú Tạ <anhtu51195@gmail.com>
2021-01-23 14:24:05 +0700

    Update BootKafka: change topic config

--- BootKafka/src/main/java/hello/listener/StartupProducer.java
+++ BootKafka/src/main/java/hello/listener/StartupProducer.java
@@ -4,33 +4,23 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 /*
- * Class StartupListener thực hiện implements ApplicationRunner nên nó sẽ được chạy hàm run ngay khi
- * start project
+ * Class StartupProducer thực hiện implements ApplicationRunner nên nó sẽ được chạy hàm run ngay khi
+ * start project. Khi start project, Producer này sẽ được tạo và liên tục send message tới topic1
  */
 @Component
-public class StartupListener implements ApplicationRunner {
+public class StartupProducer implements ApplicationRunner {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendMessage(String msg) {
-        kafkaTemplate.send("topic1", msg);
-    }
-
-    @KafkaListener(topics = "topic1", groupId = "group-id")
-    public void listen(String message) {
-        System.out.println("Received Message in group - group-id: " + message);
-    }
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
         for (int i = 0; i < 1000; i++) {
-            sendMessage("Now: " + new Date());
+            kafkaTemplate.send("topic1", "Now: " + new Date());
             Thread.sleep(2000);
         }
     }
